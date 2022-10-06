68ec40b51cb857f233daa53d7a44e84ef879d032
swetharnaik <swetharnaik.mec@gmail.com>
2022-10-07 00:39:50 +0200

    enable custom sql scripts to run

---
+++ src/main/resources/meh.sql
@@ -0,0 +1,5 @@
+INSERT INTO countries (id, alpha2code, name) VALUES (1, 'DE', 'USA');
+INSERT INTO countries (id, alpha2code, name) VALUES (2, 'DE', 'France');
+INSERT INTO countries (id, alpha2code, name) VALUES (3, 'DE', 'Brazil');
+INSERT INTO countries (id, alpha2code, name) VALUES (4, 'DE', 'Italy');
+INSERT INTO countries (id, alpha2code, name) VALUES (5, 'DE', 'Canada');
