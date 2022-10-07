ffc336d7b8c2ac6db17cf05289132c1da8ebc7b9
swetharnaik <swetharnaik.mec@gmail.com>
2022-10-07 23:35:49 +0200

    add country by name endpoint

--- src/main/java/com/example/planner/Controller.java
+++ src/main/java/com/example/planner/Controller.java
@@ -5,6 +5,7 @@ import com.example.planner.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,12 @@ public class Controller {
     CountryService countryService;
 
     @GetMapping("/countries/{id}")
-    Country getCountry(@PathVariable Long id) {
-        return countryService.getCountry(id);
+    Country getCountryById(@PathVariable Long id) {
+        return countryService.getCountryById(id);
+    }
+
+    @GetMapping("/countries")
+    Country getCountryByName(@RequestParam String name) {
+        return countryService.getCountryByName(name);
     }
 }
--- src/main/java/com/example/planner/service/CountryService.java
+++ src/main/java/com/example/planner/service/CountryService.java
@@ -6,13 +6,20 @@ import com.example.planner.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CountryService {
 
     @Autowired
     CountryRepository countryRepository;
 
-    public Country getCountry(Long id) {
-        return countryRepository.findById(new CountryKey(id, "DE")).get();
+    public Country getCountryById(Long id) {
+        Optional<Country> optionalCountry = countryRepository.findById(new CountryKey(id, "DE"));
+        return optionalCountry.orElseGet(Country::new);
+    }
+
+    public Country getCountryByName(String name) {
+        return countryRepository.findByName(name);
     }
 }
