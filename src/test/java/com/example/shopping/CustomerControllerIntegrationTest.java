11d4421d0291d94e8e0b64f64d00582a079ec6d5
swetharnaik <swetharnaik.mec@gmail.com>
2022-10-08 14:49:32 +0200

    update test cases

---
+++ src/test/java/com/example/shopping/CustomerControllerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.example.shopping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.ResourceUtils;
+
+import java.nio.file.Files;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void returnSuccessForGetCountryById() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .json(Files.readString(ResourceUtils
+                                .getFile("classpath:testresult/customer-by-id.json").toPath())));
+    }
+
+
+}
