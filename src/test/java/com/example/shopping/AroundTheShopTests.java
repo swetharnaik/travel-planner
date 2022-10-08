d2e57638dabf54b13d408ceb71c544d7330a8ea7
swetharnaik <swetharnaik.mec@gmail.com>
2022-10-08 14:49:32 +0200

    update test cases

--- src/test/java/com/example/shopping/AroundTheShopTests.java
+++ src/test/java/com/example/shopping/AroundTheShopTests.java
@@ -1,10 +1,10 @@
-package com.example.planner;
+package com.example.shopping;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RoamAroundTheWorldApplicationTests {
+class AroundTheShopTests {
 
 	@Test
 	void contextLoads() {
