732cab47e939c0ddf8061b25325e9384166c79c4
swetharnaik <swetharnaik.mec@gmail.com>
2022-10-10 16:02:19 +0200

    add jpql query

--- src/main/java/com/example/shopping/repository/OrderRepository.java
+++ src/main/java/com/example/shopping/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package com.example.shopping.repository;
 
 import com.example.shopping.models.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    @Query("SELECT o from Order o JOIN o.items i group by o.id having count(o.id)=:itemCount")
+    List<Order> getOrdersFromItemSize(Long itemCount);
 }
